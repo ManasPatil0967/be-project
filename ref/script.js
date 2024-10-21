5b5e1665406ba4c64e148c5ff0fdd06a50c2ba7b
ManasPatil0967 <manaspatil0967@gmail.com>
2024-10-21 10:05:40 +0530

    nistkat del, script update

--- ref/script.js
+++ ref/script.js
@@ -1,6 +1,9 @@
 const ffi = require('ffi-napi');
 const fs = require('fs');
+const process = require('process');
 
+const data = "kyber768-data.csv";
+const NTESTS = 1000;
 
 const kyber = ffi.Library('./libkyber.so', {
   'generate_keypair': ['int', ['string', 'string']], 
@@ -14,12 +17,20 @@ const skFile = 'secret_key.bin';
 const genResult = kyber.generate_keypair(pkFile, skFile);
 console.log("Keypair generation result: ", genResult);
 
-
-const ctFile = 'ciphertext.bin';
-const ssEncFile = 'shared_secret_enc.bin';
-const encResult = kyber.encrypt(pkFile, ctFile, ssEncFile);
-console.log("Encryption result: ", encResult);
-
+for (let i = 0; i < NTESTS; i++) {
+    let time = process.hrtime();
+    let mem = process.memoryUsage().heapUsed;
+    let start = time[0] * 1000 + time[1] / 1000000;
+    const ctFile = 'ciphertext.bin';
+    const ssEncFile = 'shared_secret_enc.bin';
+    const encResult = kyber.encrypt(pkFile, ctFile, ssEncFile);
+    time = process.hrtime();
+    let end = time[0] * 1000 + time[1] / 1000000;
+    mem = process.memoryUsage().heapUsed - mem;
+    // add to data file
+    fs.appendFileSync(data, `${end - start},${mem}\n`);
+    console.log("Encryption result: ", encResult);
+}
 
 const ssDecFile = 'shared_secret_dec.bin';
 const decResult = kyber.decrypt(skFile, ctFile, ssDecFile);
