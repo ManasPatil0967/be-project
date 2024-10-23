8e66a36808f2b7af60e0434b91d8a68e91f77406
ManasPatil0967 <manaspatil0967@gmail.com>
2024-10-23 10:38:47 +0530

    added extension dir

---
+++ extension/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const path = require("path")
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
