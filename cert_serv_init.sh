6a774a6363840cdc0f3706034f9c85ca676f7a4b
ManasPatil0967 <manaspatil0967@gmail.com>
2024-11-06 13:46:00 +0530

    server script fix

--- cert_serv_init.sh
+++ cert_serv_init.sh
@@ -3,4 +3,4 @@
 /usr/local/ssl/bin/openssl genpkey -algorithm dilithium3 -out dilithium3_srv.key
 /usr/local/ssl/bin/openssl req -new -newkey dilithium3 -keyout dilithium3_srv.key -out dilithium3_srv.csr -nodes -subj "/CN=test server" -config /usr/local/ssl/openssl.cnf
 /usr/local/ssl/bin/openssl x509 -req -in dilithium3_srv.csr -out dilithium3_srv.crt -CA dilithium3_CA.crt -CAkey dilithium3_CA.key -CAcreateserial -days 365
-/usr/local/ssl/bin/openssl s_server -cert /home/kyber/kyber_cert/dilithium3_srv.crt -key /home/kyber/kyber_cert/dilithium3_srv.key -www -tls1_3 -groups kyber768:frodo640shake -debug -security_debug_verbose -provider oqsprovider
+/usr/local/ssl/bin/openssl s_server -cert dilithium3_srv.crt -key dilithium3_srv.key -www -tls1_3 -groups kyber768:frodo640shake -debug -security_debug_verbose -provider oqsprovider
